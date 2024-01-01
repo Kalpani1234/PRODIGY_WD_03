ca96414ef4d20bb59496a457377a0461cf102a02
Kalpani1234 <Kalpani4lk@gmail.com>
2024-01-01 12:21:12 +0530

    create function handleCellPlayed

--- script.js
+++ script.js
@@ -29,3 +29,20 @@ function handleCellClick(clickedCellEvent) {
     handleCellPlayed(clickedCell, clickedCellIndex);
     handleResultValidation();
 }
+
+function handleCellPlayed(clickedCellcurrentPlayer, clickedCellIndexcurrentPlayer) {
+    
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
