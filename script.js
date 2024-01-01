4a5b556712aee81b4b2218917db78fff84988aa1
Kalpani1234 <Kalpani4lk@gmail.com>
2024-01-01 10:47:46 +0530

    do some changes

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ let currentPlayer = "X";
 
 let gameState = ["", "", "", "", "", "", "", "", ""];
 
-const winningMessage = () => `Player ${currentPlayer} has won!`;
-const drawMessage = () => `Game ended in a draw!`;
+const winningMessage = () => `Player ${currentPlayer} is the winner of this match, Congrats to him!`;
+const drawMessage = () => `Match ended in a draw!`;
 const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
 
 statusDisplay.innerHTML = currentPlayerTurn();
